030de42dceac9efb130da5145b06126fd4679dee
Cameron Burrows <cburrows@ufl.edu>
2022-04-02 17:25:05 -0400

    added map and merged eric

--- front-end/src/pages/main.tsx
+++ front-end/src/pages/main.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Component } from 'react';
+import { useEffect, useState } from "react";
 import { GoMarkGithub } from "react-icons/go";
-import image from "./images/algae.jpg"; 
+import { loadMapApi } from "../utils/googleMapsUtils";
+import Map from "../map";
 
 export default function Main() {
   const [scriptLoaded, setScriptLoaded] = useState(false);
@@ -14,24 +13,30 @@ export default function Main() {
   }, []);
 
   return (
-    <div className = "Main">
-      <div className = "Header">
+    <div className="Main">
+      <div className="Header">
         <h1>Algae Attack</h1>
       </div>
       <div className="About">
-        <h3 className = "AboutHeader">About </h3>
-        <p className = "AboutParagraph"> Algae attack is a terrible website developed by dustin a
-           s s s sasssssssssssssss s s s s ss s s s s it simulates justin aaaa  a   sasdas das
-           dasdasdasdasdasdd sad as das das dasd asd asdsa asdasdasssssssssssssssssssssssssss
-           sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssdasd</p>
+        <h3 className="AboutHeader">About </h3>
+        <p className="AboutParagraph">
+          {" "}
+          Algae attack is a terrible website developed by dustin a s s s
+          sasssssssssssssss s s s s ss s s s s it simulates justin aaaa a sasdas
+          das dasdasdasdasdasdd sad as das das dasd asd asdsa
+          asdasdasssssssssssssssssssssssssss
+          sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssdasd
+        </p>
       </div>
       {scriptLoaded && (
         <Map mapType={google.maps.MapTypeId.ROADMAP} mapTypeControl={true} />
       )}
-      <div className = "FooterDiv">
+      <div className="FooterDiv">
         <footer>
           <GoMarkGithub />
-            <a target="_blank" href="https://github.com/jmho/algaeattack">GitHub</a>
+          <a target="_blank" href="https://github.com/jmho/algaeattack">
+            GitHub
+          </a>
         </footer>
       </div>
     </div>
