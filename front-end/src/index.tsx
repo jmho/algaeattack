8e2d50e21a36d15a3c99d5b6213dff7e4706b071
Cameron Burrows <cburrows@ufl.edu>
2022-04-02 14:25:09 -0400

    added routes

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/main";
+import NotFound from "./pages/404";
+import reportWebVitals from "./reportWebVitals";
+
+const homepageTitle = "test title";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
