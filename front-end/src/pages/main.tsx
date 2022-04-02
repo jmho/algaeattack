c268576376c02e4062a31bf4bd9709f44a3a7068
EricS222 <75048519+EricS222@users.noreply.github.com>
2022-04-02 18:56:17 -0400

    Front End Changes

--- front-end/src/pages/main.tsx
+++ front-end/src/pages/main.tsx
@@ -13,31 +13,35 @@ export default function Main() {
   }, []);
 
   return (
-    <div className="Main">
-      <div className="Header">
-        <h1>Algae Attack</h1>
+    <div>
+      <div className = "Center">
+        <div className="Header">
+          <h1>Algae Attack</h1>
+        </div>
       </div>
-      <div className="About">
-        <h3 className="AboutHeader">About </h3>
-        <p className="AboutParagraph">
-          {" "}
-          Algae attack is a terrible website developed by dustin a s s s
-          sasssssssssssssss s s s s ss s s s s it simulates justin aaaa a sasdas
-          das dasdasdasdasdasdd sad as das das dasd asd asdsa
-          asdasdasssssssssssssssssssssssssss
-          sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssdasd
-        </p>
+        <div className="About">
+          <h3 className="AboutHeader">About </h3>
+          <p className="AboutParagraph">
+            {" "}
+            Algae attack is a terrible website developed by dustin a s s s
+            sasssssssssssssss s s s s ss s s s s it simulates justin aaaa a sasdas
+            das dasdasdasdasdasdd sad as das das dasd asd asdsa
+            asdasdasssssssssssssssssssssssssss
+            sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssdasd
+          </p>
       </div>
       {scriptLoaded && (
         <Map mapType={google.maps.MapTypeId.ROADMAP} mapTypeControl={true} />
       )}
-      <div className="FooterDiv">
-        <footer>
-          <GoMarkGithub />
-          <a target="_blank" href="https://github.com/jmho/algaeattack">
-            GitHub
-          </a>
-        </footer>
+      <div className = "Center">
+        <div className="FooterDiv">
+          <footer>
+            <GoMarkGithub />
+            <a target="_blank" href="https://github.com/jmho/algaeattack">
+              GitHub
+            </a>
+          </footer>
+        </div>
       </div>
     </div>
   );
