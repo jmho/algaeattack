2f9d3ee41eb4cf9df93de5d1a192569d8bd0024f
EricS222 <75048519+EricS222@users.noreply.github.com>
2022-04-02 17:15:12 -0400

    added map

--- front-end/src/pages/main.tsx
+++ front-end/src/pages/main.tsx
@@ -1,15 +1,8 @@
-import { useEffect, useState } from "react";
-import { loadMapApi } from "../utils/googleMapsUtils";
-import Map from "../Map/map";
-
-function Header() {
-  return (
-    <div className="header">
-      <h1 className="title">Algae Attack</h1>
-      <h1 className="title">Algae 2</h1>
-    </div>
-  );
-}
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Component } from 'react';
+import { GoMarkGithub } from "react-icons/go";
+import image from "./images/algae.jpg"; 
 
 export default function Main() {
   const [scriptLoaded, setScriptLoaded] = useState(false);
@@ -21,10 +14,26 @@ export default function Main() {
   }, []);
 
   return (
-    <div className="page">
+    <div className = "Main">
+      <div className = "Header">
+        <h1>Algae Attack</h1>
+      </div>
+      <div className="About">
+        <h3 className = "AboutHeader">About </h3>
+        <p className = "AboutParagraph"> Algae attack is a terrible website developed by dustin a
+           s s s sasssssssssssssss s s s s ss s s s s it simulates justin aaaa  a   sasdas das
+           dasdasdasdasdasdd sad as das das dasd asd asdsa asdasdasssssssssssssssssssssssssss
+           sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssdasd</p>
+      </div>
       {scriptLoaded && (
         <Map mapType={google.maps.MapTypeId.ROADMAP} mapTypeControl={true} />
       )}
+      <div className = "FooterDiv">
+        <footer>
+          <GoMarkGithub />
+            <a target="_blank" href="https://github.com/jmho/algaeattack">GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
